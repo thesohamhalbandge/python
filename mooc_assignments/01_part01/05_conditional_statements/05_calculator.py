725703ffd464004a86d51123dd51cdefc851cbfa
Soham Halbandge <batman@Sohams-MacBook-Air.local>
2025-03-04 08:20:23 +0530

    commit

---
+++ mooc_assignments/01_part01/05_conditional_statements/05_calculator.py
@@ -0,0 +1,40 @@
+'''
+Please write a program which asks the user for two numbers and an operation. 
+If the operation is add, multiply or subtract, the program should calculate 
+and print out the result of the operation with the given numbers. 
+If the user types in anything else, the program should print out nothing.
+
+Some examples of expected behaviour:
+
+Sample output
+Number 1: 10
+Number 2: 17
+Operation: add
+
+10 + 17 = 27
+
+Sample output
+Number 1: 4
+Number 2: 6
+Operation: multiply
+
+4 * 6 = 24
+
+Sample output
+Number 1: 4
+Number 2: 6
+Operation: subtract
+
+4 - 6 = -2
+'''
+
+num1 = int(input("Number 1:"))
+num2 = int(input("Number 2:"))
+operation = input("Operation:")
+
+if operation == 'add':
+    print(num1, '+', num2, '=', num1 + num2)
+elif operation == 'subtract':
+    print(num1, '-', num2, '=', num1 - num2)
+elif operation == 'multiply':
+    print(num1, '*', num2, '=', num1 * num2)
